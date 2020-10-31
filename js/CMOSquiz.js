6a6b904a383c9a659701fbc1b488f2c0a48cf796
Tony Bo <bochuangjie@gmail.com>
2020-10-31 14:32:12 +0800

    append result to google sheet

--- js/CMOSquiz.js
+++ js/CMOSquiz.js
@@ -1,3 +1,12 @@
+document.onload=this.onload=function(){
+    handleClientLoad()
+    };
+document.onreadystatechange= function()
+    {
+        if (this.readyState === 'complete') {
+        this.onload()
+        }
+    };
 const modal = document.getElementById('modal');
 const modalClose = document.getElementById('submit');
 const name = document.getElementById('name');
@@ -37,7 +46,7 @@ function checkName(event){
 }
 function checkMatri(event){
     var matric = event.currentTarget;
-    var pos = matric.value.search(/^[a-zA-Z0-9]+$/);
+    var pos = matric.value.search(/^[U]\d{7}[a-zA-z]$/);
     if(pos !=0){
         alert("The value you have entered (" + matric.value + ") is not in the correct form. \n"+ "Only alphabets and numbers  are allowed. ");
         matric.focus();
@@ -193,4 +202,120 @@ function clearStatusClass(element){
     element.classList.remove('wrong');
     element.classList.remove('correct');
 }
+//Google authentication and Google sheet append
+
+ // Client ID and API key from the Developer Console
+ var CLIENT_ID = '347108569823-16ajosbbbj1n8bc1vkj5lp429vehm1sg.apps.googleusercontent.com';
+ var API_KEY = 'AIzaSyAGHFzpIY_RnTixA82GMXAw3SMMaD_Jh_Q';
+
+ // Array of API discovery doc URLs for APIs used by the quickstart
+ var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+ // Authorization scopes required by the API; multiple scopes can be
+ // included, separated by spaces.
+ var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+
+ var authorizeButton = document.getElementById('start');
+ var submitButton= document.getElementById('proceed');
+
+ /**
+  *  On load, called to load the auth2 library and API client library.
+  */
+ function handleClientLoad() {
+   gapi.load('client:auth2', initClient);
+ }
+
+ /**
+  *  Initializes the API client library and sets up sign-in state
+  *  listeners.
+  */
+ function initClient() {
+   gapi.client.init({
+     apiKey: API_KEY,
+     clientId: CLIENT_ID,
+     discoveryDocs: DISCOVERY_DOCS,
+     scope: SCOPES
+   }).then(function () {
+     // Listen for sign-in state changes.
+     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+     // Handle the initial sign-in state.
+     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+     authorizeButton.onclick = handleAuthClick;
+    //  signoutButton.onclick = handleSignoutClick;
+   }, function(error) {
+     appendPre(JSON.stringify(error, null, 2));
+   });
+ }
+
+ /**
+  *  Called when the signed in status changes, to update the UI
+  *  appropriately. After a sign-in, the API is called.
+  */
+ function updateSigninStatus(isSignedIn) {
+
+ }
+
+ /**
+  *  Sign in the user upon button click.
+  */
+ function handleAuthClick(event) {
+   gapi.auth2.getAuthInstance().signIn();
+ }
+
+ /**
+  *  Sign out the user upon button click.
+  */
+ function handleSignoutClick(event) {
+   gapi.auth2.getAuthInstance().signOut();
+ }
+
+ /**
+  * Append a pre element to the body containing the given message
+  * as its text node. Used to display the results of the API call.
+  *
+  * @param {string} message Text to be placed in pre element.
+  */
+ function appendPre(message) {
+   var pre = document.getElementById('content');
+   var textContent = document.createTextNode(message + '\n');
+   pre.appendChild(textContent);
+ }
+
+ /**
+  * Print the names and majors of students in a sample spreadsheet:
+  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+  */
+
+
+
+ const test = 'CMOS';
+ const date = new Date();
+ const day = date.getDate();
+ const month = date.getMonth()+1;
+ const year = date.getFullYear();
+
+ function listMajors() {
+   var values = [
+   [
+     // Cell values ...
+     nameSheet,matriSheet,score,day,month,year
+   ],
+   // Additional rows ...
+ ];
+ var body = {
+   values: values
+ };
+ gapi.client.sheets.spreadsheets.values.append({
+   spreadsheetId: '1Y6M6GkTVlZFPvUy-B5ORNbKZJrEb0bAKwoV7ykklirQ',
+   range: 'A:Z',
+   valueInputOption: 'USER_ENTERED',
+   resource: body
+ }).then((response) => {
+   var result = response.result;
+   console.log(`${result.updatedCells} cells updated.`);
+ });
+
+ }
+   
 
